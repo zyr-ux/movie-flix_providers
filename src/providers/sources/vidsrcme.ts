df9c75ed9410d83f8c70b7fb3044113985780a6b
Zyrux <152474063+zyr-ux@users.noreply.github.com>
2024-06-16 01:22:29 +0530

    Update vidsrcme.ts

--- src/providers/sources/vidsrcme.ts
+++ src/providers/sources/vidsrcme.ts
@@ -1,1 +1,43 @@
+import { flags } from '@/entrypoint/utils/targets';
+import { SourcererOutput, makeSourcerer } from '@/providers/base';
+import { MovieScrapeContext, ShowScrapeContext } from '@/utils/context';
+import { NotFoundError } from '@/utils/errors';
 
+const universalScraper = async (ctx: MovieScrapeContext | ShowScrapeContext) => {
+  const apiRes: { url: string } = await ctx.fetcher<{ url: string }>(
+    https://vidsrcme.wafflehacker.io/$%7Bctx.media.type === 'movie' ? 'movie' : 'tv'}`,
+    {
+      query: {
+        tmdbId: ctx.media.tmdbId,
+        ...(ctx.media.type === 'show' && {
+          season: ctx.media.season.number.toString(),
+          episode: ctx.media.episode.number.toString(),
+        }),
+      },
+    },
+  );
+
+  if (!apiRes.url) throw new NotFoundError('No stream found.');
+
+  return {
+    embeds: [],
+    stream: [
+      {
+        id: 'primary',
+        type: 'hls',
+        playlist: apiRes.url,
+        captions: [],
+        flags: [flags.CORS_ALLOWED],
+      },
+    ],
+  } as SourcererOutput;
+};
+
+export const vidsrcWaffleHackerScraper = makeSourcerer({
+  id: 'vidsrc-wafflehacker',
+  name: 'Vidsrc',
+  rank: 135,
+  flags: [flags.CORS_ALLOWED],
+  scrapeMovie: universalScraper,
+  scrapeShow: universalScraper,
+});
