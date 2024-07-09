183edd099eb5c5d07955e730b1e97a3ce3ef9a92
TPN <tpn@ainta.dev>
2024-07-09 14:47:28 +0100

    Update ranks again

--- src/providers/embeds/whvx.ts
+++ src/providers/embeds/whvx.ts
@@ -7,12 +7,12 @@ const providers = [
   {
     id: 'nova',
     name: 'VidBinge Nova',
-    rank: 601,
+    rank: 701,
   },
   {
     id: 'astra',
     name: 'VidBinge Astra',
-    rank: 600,
+    rank: 700,
   },
 ];
 
